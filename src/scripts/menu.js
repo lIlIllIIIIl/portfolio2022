1067e465065ca93e948128edcdf0432407ffca79
lIlIllIIIIl <alexis.kessab@orange.fr>
2022-03-10 16:28:03 +0100

    scroll, about init, etc.

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,13 +1,17 @@
 const page = document.querySelector("body");
 
 const menu = page.querySelector(".menu")
-const menu_click = menu.querySelector(".menu_title h5")
+const menu_click = menu.querySelector(".menu_title")
+const menu_square = menu.querySelector(".menu_square")
 
 menu_click.addEventListener("click", (e)=>{
 	if (menu.classList[1] === "menu_close"){
 		menu.classList.replace("menu_close", "menu_open")
+		menu_square.style.transform="rotate(90deg)"
+
 		// console.log(menu.classList);
 	}else if(menu.classList[1] === "menu_open"){
 		menu.classList.replace("menu_open", "menu_close")
+		menu_square.style.transform="rotate(0)"
 	}
 })
---
+++ src/scripts/workScroll.js
@@ -0,0 +1,71 @@
+const { gsap } = require("gsap/dist/gsap");
+const { ScrollTrigger } = require("gsap/dist/ScrollTrigger");
+    
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
+
+
+let panelsSection = document.querySelector(".work_global"),
+	panelsContainer = document.querySelector("#works"),
+	tween;
+document.querySelectorAll(".anchor").forEach(anchor => {
+	anchor.addEventListener("click", function(e) {
+		e.preventDefault();
+		let targetElem = document.querySelector(e.target.getAttribute("href")),
+			y = targetElem;
+		if (targetElem && panelsContainer.isSameNode(targetElem.parentElement)) {
+			let totalScroll = tween.scrollTrigger.end - tween.scrollTrigger.start,
+				totalMovement = (panels.length - 1) * targetElem.offsetWidth;
+			y = Math.round(tween.scrollTrigger.start + (targetElem.offsetLeft / totalMovement) * totalScroll);
+		}
+		gsap.to(window, {
+			scrollTo: {
+				y: y,
+				autoKill: true
+			},
+			duration: 1
+		});
+	});
+});
+
+/* Panels */
+const panels = gsap.utils.toArray("#works .work_project");
+tween = gsap.to(panels, {
+	xPercent: -100 * ( panels.length - 1 ),
+	ease: "none",
+	scrollTrigger: {
+		// trigger: "#works",
+		trigger: ".container",
+		pin: true,
+		start: "center center",
+        // markers: true,
+		scrub: 1,
+		// snap: {
+		// 	snapTo: 1 / (panels.length - 1),
+		// 	inertia: false,
+		// 	duration: {min: 0.1, max: 0.1}
+		// },
+		end: () =>  "+=" + (panelsContainer.offsetWidth - innerWidth)
+	}
+});
+
+
+
+
+
+
+
+// let sections = gsap.utils.toArray(".work_project");
+// console.log(sections);
+// gsap.to(sections, {
+//   xPercent: -100 * (sections.length - 1),
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".container",
+//     pin: true,
+//     scrub: 1,
+//     snap: 1 / (sections.length - 1),
+//     // base vertical scrolling on how wide the container is so it feels more natural.
+//     end: "+=3500",
+//   }
+// });
+
