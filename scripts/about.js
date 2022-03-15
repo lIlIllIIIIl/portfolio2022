3f55da7f036cbf1a47e487f0a637282348d85ae3
Alexis Kessab <alexis.kessab@orange.fr>
2022-03-15 23:16:46 +0100

    about's direct animations (row 1)

--- scripts/about.js
+++ scripts/about.js
@@ -22,61 +22,82 @@ const foodRowNum = Math.round(foodImage.clientHeight/14)-1;
 
 
 function createHoverLines (){
-    for (i = 0 ; i < gameRowNum ; i++){
-        let newSpan = document.createElement("span")
-        newSpan.classList.add("game_row")
-        gameHover.appendChild(newSpan);
+	for (i = 0 ; i < gameRowNum ; i++){
+		let newSpan = document.createElement("span")
+		newSpan.classList.add("game_row")
+		gameHover.appendChild(newSpan);
 
-    }
+	}
 
-    for (i = 0 ; i < mangaColNum ; i++){
-        let newSpan = document.createElement("span")
-        newSpan.classList.add("manga_column")
-        mangaHover.appendChild(newSpan);
-    }
+	for (i = 0 ; i < mangaColNum ; i++){
+		let newSpan = document.createElement("span")
+		newSpan.classList.add("manga_column")
+		mangaHover.appendChild(newSpan);
+	}
 
-    for (i = 0 ; i < foodRowNum ; i++){
-        let newSpan = document.createElement("span")
-        newSpan.classList.add("food_row")
-        foodHover.appendChild(newSpan);
-    }
+	for (i = 0 ; i < foodRowNum ; i++){
+		let newSpan = document.createElement("span")
+		newSpan.classList.add("food_row")
+		foodHover.appendChild(newSpan);
+	}
 }
 
 createHoverLines()
 
 let gameRows = game.querySelectorAll(".game_row")
 let mangaColumns = manga.querySelectorAll(".manga_column")
-let foodRows = manga.querySelectorAll(".food_row")
+let foodRows = food.querySelectorAll(".food_row")
 
-gameHover.addEventListener("mouseenter", () =>{
-    for (let row of gameRows){
-        row.style.display = "inline"
-    }
+function replaceHoriz(elem){
+	elem.style.display = "none"
+	elem.style.transform = "translate(-100%)"
+}
+function replaceVert(elem){
+	elem.style.display = "none"
+	elem.style.transform = "translateY(100%)"
+}
+function display(elem){
+	elem.style.display = "inline"
+}
+	// NE PAS DEPLACER LES LIGNES UNE PAR UNE MAIS TTE LA DIV
+game.addEventListener("mouseenter", () =>{
+	for (let row of gameRows){
+		// row.style.display = "inline"
+		// row.style.transform = "translate(100%)"
+	}
 })
-gameHover.addEventListener("mouseleave", () =>{
-    for (let row of gameRows){
-        row.style.display = "none"
-    }
+game.addEventListener("mouseleave", () =>{
+	for (let row of gameRows){
+		row.style.transform = "translate(200%)"
+		setTimeout(replaceHoriz, 300, row)
+		setTimeout(display, 400, row)
+	}
 })
 
-mangaHover.addEventListener("mouseenter", () =>{
-    for (let column of mangaColumns){
-        column.style.display = "inline"
-    }
+manga.addEventListener("mouseenter", () =>{
+	for (let column of mangaColumns){
+		// column.style.display = "inline"
+		// column.style.transform = "translateY(-100%)"
+	}
 })
-mangaHover.addEventListener("mouseleave", () =>{
-    for (let column of mangaColumns){
-        column.style.display = "none"
-    }
+manga.addEventListener("mouseleave", () =>{
+	for (let column of mangaColumns){
+		column.style.transform = "translateY(-200%)"
+		setTimeout(replaceVert, 300, column)
+		setTimeout(display, 400, column)
+	}
 })
 
-foodHover.addEventListener("mouseenter", () =>{
-    for (let row of foodRows){
-        row.style.display = "inline"
-    }
+food.addEventListener("mouseenter", () =>{
+	for (let row of foodRows){
+		// row.style.display = "inline"
+		// row.style.transform = "translate(100%)"
+	}
 })
-foodHover.addEventListener("mouseleave", () =>{
-    for (let row of foodRows){
-        row.style.display = "none"
-    }
+food.addEventListener("mouseleave", () =>{
+	for (let row of foodRows){
+		row.style.transform = "translate(200%)"
+		setTimeout(replaceHoriz, 300, row)
+		setTimeout(display, 400, row)
+	}
 })
