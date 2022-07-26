be1595015bb243c3efa05ac7d089db35070c1d42
lIlIllIIIIl <57530336+lIlIllIIIIl@users.noreply.github.com>
2022-07-26 19:40:31 +0200

    about.2
    image management

--- scripts/aboutClick.js
+++ scripts/aboutClick.js
@@ -5,12 +5,27 @@ const aboutOpen = document.querySelector(".aboutContainer-open")
 const titleOpen = aboutOpen.querySelector(".aboutTitle-open")
 const imageOpen = aboutOpen.querySelector(".aboutImage-open")
 const textOpen = aboutOpen.querySelector(".aboutText-open")
+
+let isOpen = 0
+
+document.addEventListener("click", function(e){
+	if (isOpen === 1){
+		isOpen = 0
+		aboutOpen.style.display="none"
+		about.style.display="flex"
+	}
+})
+
 images.forEach(elem =>{
 	elem.addEventListener("click", function(e){
-		about.style.display="none"
-		aboutOpen.style.display="block"
-		titleOpen.innerHTML = elem.alt
-		imageOpen.src = elem.src
-		textOpen.innerHTML = elem.ariaValueMax
+		if (isOpen === 0){
+			console.log('in');
+			about.style.display="none"
+			aboutOpen.style.display="block"
+			titleOpen.innerHTML = elem.alt
+			imageOpen.src = elem.src
+			textOpen.innerHTML = elem.ariaValueMax
+			isOpen = 1
+		}
 	})
 })
