931c35c6eca769ab211d680b901c287a01233e2f
lIlIllIIIIl <57530336+lIlIllIIIIl@users.noreply.github.com>
2022-07-27 11:35:56 +0200

    .

--- scripts/aboutClick.js
+++ scripts/aboutClick.js
@@ -1,3 +1,5 @@
+const fullPage = document.querySelector(".mainPage")
+
 const about = document.querySelector(".aboutContainer")
 const images = about.querySelectorAll(".aboutImage")
 
@@ -6,26 +8,22 @@ const titleOpen = aboutOpen.querySelector(".aboutTitle-open")
 const imageOpen = aboutOpen.querySelector(".aboutImage-open")
 const textOpen = aboutOpen.querySelector(".aboutText-open")
 
-let isOpen = 0
 
-document.addEventListener("click", function(e){
-	if (isOpen === 1){
-		isOpen = 0
-		aboutOpen.style.display="none"
-		about.style.display="flex"
-	}
+aboutOpen.addEventListener("click", function(e){
+	console.log("out");
+	aboutOpen.style.display="none"
+	about.style.display="flex"
 })
 
 images.forEach(elem =>{
 	elem.addEventListener("click", function(e){
-		if (isOpen === 0){
 			console.log('in');
 			about.style.display="none"
-			aboutOpen.style.display="block"
+			aboutOpen.style.display="flex"
 			titleOpen.innerHTML = elem.alt
-			imageOpen.src = elem.src
+			let src = elem.src.replace("small", "large")
+			console.log(src);
+			imageOpen.src = src
 			textOpen.innerHTML = elem.ariaValueMax
-			isOpen = 1
-		}
 	})
 })
